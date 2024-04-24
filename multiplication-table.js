2f42f99dade6a9ec724172bbbccf208f0dd5ad45
Azegza <136713537+Azegza@users.noreply.github.com>
2024-04-24 10:56:53 +0100

    Create multiplication-table.js

---
+++ multiplication-table.js
@@ -0,0 +1,10 @@
+// Multiplication table  0-10
+// nested loops 
+// outter loop
+
+for( var firstNum = 0; firstNum<=5; firstNum++){
+// inner loop
+  for ( var secondNum=0; secondNum<=4; secondNum ++){
+       console.log( firstNum + " x " + secondNum + " = " + firstNum*secondNum);
+}
+}
